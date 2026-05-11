268c955991f7113ded5eb6545d653213837dccb1
Felipe González <gonzalezpobletefelipe@gmail.com>
2026-05-11 14:34:45 -0400

    ola

--- pioneer/config/udev_scripts/create_udev_rules.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-RULES=~/Documents/docker_images/pioneer/config/udev_scripts/99-pioneer.rules
-
-echo "Installing udev rules from $RULES"
-sudo cp "$RULES" /etc/udev/rules.d/
-sudo udevadm control --reload-rules && sudo udevadm trigger
-
-echo "Done!"
--- pioneer/config/udev_scripts/delete_udev_rules.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Deleting bender udev rules"
-sudo rm -f /etc/udev/rules.d/99-bender.rules
-sudo udevadm control --reload-rules && sudo udevadm trigger
-echo "Done!"
